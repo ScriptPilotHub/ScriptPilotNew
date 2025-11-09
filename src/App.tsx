73a50b096e1091289adcaf4a73ecc38c089933f1
ScriptPilotHub <Dtackman23@gmail.com>
2025-11-09 17:01:34 -0600

    Actually rebuild the entire site from scratch

--- src/App.tsx
+++ src/App.tsx
@@ -4,31 +4,20 @@ import { Footer } from './components/layout/Footer';
 import { Home } from './components/pages/Home';
 import { Services } from './components/pages/Services';
 import { About } from './components/pages/About';
-import { FAQ } from './components/pages/FAQ';
 import { Contact } from './components/pages/Contact';
-import { PaymentPortal } from './components/pages/PaymentPortal';
 import { usePathname } from './hooks/usePathname';
 
 function App() {
   const currentRoute = usePathname();
 
   const renderPage = () => {
-    const baseRoute = currentRoute.split('/')[0];
-
-    switch (baseRoute) {
-      case 'home':
-      case '':
-        return <Home />;
+    switch (currentRoute) {
       case 'services':
         return <Services />;
       case 'about':
         return <About />;
-      case 'faq':
-        return <FAQ />;
       case 'contact':
         return <Contact />;
-      case 'payment-portal':
-        return <PaymentPortal />;
       default:
         return <Home />;
     }
